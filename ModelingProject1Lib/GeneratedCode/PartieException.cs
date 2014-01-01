042cbb5e51b529e359f183f4b10aa2af3686f58a
Bruno Matry <brunomatry@gmail.com>
2014-01-02 00:21:34 +0100

    PartieException

---
+++ ModelingProject1Lib/GeneratedCode/PartieException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class PartieException : Exception {
+
+    private string _message;
+	private string _type;
+ 
+    // Propriétés (en lecture seule)
+	public string Message { get { return this._message; } }
+	public string Type { get { return this._type; } }
+ 
+    // Constructeur
+	public PartieException(string m) : base(m) {
+        this._message = m;
+		this._type = "Smallworld";
+    }
+
+	public PartieException(string m, string t) : base(m) {
+		this._message = m;
+		this._type = t;
+	}
+}

240c52999b6c1475d98f309647874a6ed7f82e0f
Bruno Matry <brunomatry@gmail.com>
2014-01-06 13:52:01 +0100

    Wrapper

--- Test/TestUnitairesWrapper.cs
+++ Test/TestUnitairesWrapper.cs
@@ -6,27 +6,24 @@ using Wrapper;
 namespace Test
 {
 	[TestClass]
-	public class TestUnitairesWrapper
-	{
+	public class TestUnitairesWrapper {
 
 		[TestMethod]
-		public void TestMethod1()
-		{
+		unsafe public void Test_GenerationCarte_1() {
+			// Creation du wrapper (carte de taille 1 x 2)
 			WrapperLib w = new WrapperLib(1, 2);
-			List<int> l = w.gen_carte(1);
-			Assert.IsTrue(l[0] == 0);
-			Assert.IsTrue(l[1] == 0);
-			Assert.IsTrue(l[2] == 0);
-			Assert.IsTrue(l[3] == 0);
+			// Generation de la carte avec un seul type de case
+			int** l = w.generer_carte(1);
+			Assert.IsTrue(l[0][0] == 0);
+			
 		}
 
 		[TestMethod]
-		public void TestWrapper2()
-		{
+		unsafe public void TestWrapper2() {
 			// Genere une carte de 3 cases avec deux types de case different
 			WrapperLib w = new WrapperLib(1, 2);
-			List<int> l = w.gen_carte(2);
-			Assert.IsTrue(l[0] == 0 || l[0] == 1);
+			int** l = w.generer_carte(2);
+			Assert.IsTrue(l[0][0] == 0 || l[0][0] == 1);
 		}
 	}
 }
--- Wrapper/Wrapper.h
+++ Wrapper/Wrapper.h
@@ -14,27 +14,15 @@ namespace Wrapper {
 		public:
 			WrapperLib(const int l, const int h){ api = Api_new(l, h); }
 			~WrapperLib(){ Api_delete(api); }
-			/**
-			 * Methode permettant de recupérer la valeur de la fonction gen_carte de l'API (librairie C++)
-			 */
-			List<int>^ gen_carte(const int ntc) {
-				int* tab = api->gen_carte(ntc);
-				List<int>^ res = gcnew List<int>();
-				int nc = api->getNbCases();
-				for (int i = 0 ; i < nc ; i++) {
-					res->Add(tab[i]);
-				}
-				return res;
-			};
-
-			List<int>^ placer_unites(const int nbJ) {
-				int* tab = api->placerUnites(nbJ);
-				List<int>^ res = gcnew List<int>();
-				for(int i = 0 ; i < (nbJ * 2) ; i++) {
-					res->Add(tab[i]);
-				}
-				return res;
-			}
+			int** generer_carte(const int ntc) { return Api_genCarteApi(api, ntc); }
+			int** placer_unites(const int nbJ) { return Api_placerUnites(api, nbJ); }
 	};
 }
 #endif
+
+	/*int* tab = api->gen_carte(ntc);
+		List<int>^ res = gcnew List<int>();
+		int nc = api->getNbCases();
+		for (int i = 0 ; i < nc ; i++) {
+			res->Add(tab[i]);
+		} */

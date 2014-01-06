688787db2cbe502e92cc7d552f4a5745e47be4d7
sami602 <selalami@insa-rennes.fr>
2014-01-06 11:30:31 +0100

    MAJ

--- WpfApplication1/Properties/AssemblyInfo.cs
+++ WpfApplication1/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // Les informations générales relatives à un assembly dépendent de 
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("WpfApplication1")]
+[assembly: AssemblyTitle("SmallWorldGraphics")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WpfApplication1")]
+[assembly: AssemblyProduct("SmallWorldGraphics")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]

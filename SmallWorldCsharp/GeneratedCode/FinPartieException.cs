8a57ecd6bacca0ea49e500d954b06c4f876daee4
Bruno Matry <brunomatry@gmail.com>
2014-01-06 16:22:14 +0100

    Exception

---
+++ SmallWorldCsharp/GeneratedCode/FinPartieException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class FinPartieException : PartieException
+{
+
+	private string _message;
+	private string _type;
+
+	// Propriétés (en lecture seule)
+	public override string Message { get { return this._message; } }
+	public string Type { get { return this._type; } }
+
+	// Constructeur
+	public FinPartieException(string m) : base(m) {
+		this._message = "La partie est terminee : " + m;
+		this._type = "Fin de partie";
+	}
+}
+

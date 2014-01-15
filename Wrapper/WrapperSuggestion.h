74ba223536ac17dc06dcada33f0a096313278c98
Bruno Matry <brunomatry@gmail.com>
2014-01-15 13:03:56 +0100

    Merge

---
+++ Wrapper/WrapperSuggestion.h
@@ -0,0 +1,19 @@
+#ifndef __WRAPPER__
+#define __WRAPPER__
+
+#include "../LibCpp/Api_suggestion.h"
+#pragma comment(lib, "../Debug/LibCpp.lib")
+
+using namespace System;
+using namespace System::Collections::Generic;
+
+namespace Wrapper {
+	public ref class WrapperLib {
+		private:
+			Api_suggestion* api;
+		public:
+			
+			/* TODO */
+	};
+}
+#endif
